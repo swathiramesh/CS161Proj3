6e3c4dec435698591591ca0cc4bdeb42a23b6a7d
Swathi Ramesh <slramesh@berkeley.edu>
2017-11-23 11:56:43 -0800

    TCP conn not established

--- proj3/common.py
+++ proj3/common.py
@@ -184,15 +184,16 @@ class PacketUtils:
         sport = random.randint(2000, 30000)
         self.send_pkt(flags="S", sport=sport)
         packet = self.get_pkt()
-        while packet == None:
-            print("ACK not received")
-            self.send_pkt(flags="S", sport=sport)
-            packet = self.get_pkt()
-        self.send_pkt(flags="A", seq=packet[TCP].ack+1, ack=packet[TCP].seq+1, payload = triggerfetch)
+        # while packet == None:
+        #     print("ACK not received")
+        #     self.send_pkt(flags="S", sport=sport)
+        #     packet = self.get_pkt()
+        self.send_pkt(flags="A", seq=packet[TCP].ack+1, ack=packet[TCP].seq+1)
         result = self.get_pkt()
         while result == None:
-            print("Final Ack")
-            self.send_pkt(flags="A", seq=packet[TCP].ack+1, ack=packet[TCP].seq+1, payload = triggerfetch)
+            self.send_pkt(flags="S", sport=sport)
+            packet = self.get_pkt()
+            self.send_pkt(flags="A", seq=packet[TCP].ack+1, ack=packet[TCP].seq+1)
             result = self.get_pkt()
         ip_addr = []
         rst_lst = []
